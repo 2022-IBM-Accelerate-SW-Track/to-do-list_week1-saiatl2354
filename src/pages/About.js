00ceb4fb67d9822118c5313bc4ef3ee7d740eca5
saiatl2354 <atluris@dickinson.edu>
2022-06-12 07:11:36 -0400

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import <profile_pic_name> from "../assets/profile_pic_name.JPG";
   
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <p>Design your About me page </p> 
+    <div>
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src={profile_pic}
+            alt="Profile Pic"
+            ></img>
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Your Name</div>
+          <div className="brief_description">
+            // Details about you goes here
+          </div>
+        </div>
+      </div>
+    </div>
+  
     )
   }
 }
